1fc67c3374eb52244cd171f8ccba967f234b89e9
Rodney Shupe <rodney@shupe.ca>
2010-11-14 11:05:52 -0800

    added control over queue/queue items

--- widgets/wSabnzbd.php
+++ widgets/wSabnzbd.php
@@ -1,9 +1,31 @@
 <?php
-$wdgtSabnzbd = array("name" => "Sabnzbd", "type" => "ajax", "block" => "sabnzbdwrapper", "call" => "widgets/wSabnzbd.php?style=w&c=15", "interval" => 10000);
+$wdgtSabnzbd = array("name" => "Sabnzbd", "type" => "ajax", "block" => "sabnzbdwrapper", "headerfunction" => "widgetSabnzbdHeader();", "call" => "widgets/wSabnzbd.php?style=w&c=15", "interval" => 10000);
 $wIndex["wSabnzbd"] = $wdgtSabnzbd;
 
-?>
-<?php
+function widgetSabnzbdHeader() {
+	echo <<< SABNZBDHEADER
+		<script type="text/javascript" language="javascript">
+		<!--
+			function cmdSabnzbd(requesturl) {
+//alert(requesturl);
+				var cmdSabnzbdRequest = new ajaxRequest();
+				cmdSabnzbdRequest.open("GET", requesturl, true);
+				cmdSabnzbdRequest.onreadystatechange = function() {
+					if (cmdSabnzbdRequest.readyState==4) {
+						if (cmdSabnzbdRequest.status==200 || window.location.href.indexOf("http")==-1) {
+							document.getElementById("sabnzbdwrapper").innerHTML = cmdSabnzbdRequest.responseText;
+						} else {
+							//alert("An error has occured making the request");
+						}
+					}
+				}
+				cmdSabnzbdRequest.send(null);
+			}
+		-->
+		</script>
+SABNZBDHEADER;
+}
+
 function sabQuery($command, $values = array()) {
 	global $saburl, $sabapikey;
 
@@ -26,38 +48,113 @@ function sabQuery($command, $values = array()) {
 }
 
 function sabStatus($count = 15) {
-	echo "<div id=\"sabnzbd\">\n";
+	global $saburl, $sabapikey;
 
-	$sabqueue = sabQuery("qstatus");
-	//echo "<br/><pre>".print_r($sabqueue)."</pre>";
+	echo "\t<div id=\"sabnzbd\">\n";
 
-	echo "\t<p>".$sabqueue["state"];
-	if ($sabqueue["state"] == "DOWNLOADING") {
-		echo " AT ".$sabqueue["speed"]."</p>\n";
-		echo "\t<p>TIMELEFT - ".$sabqueue["timeleft"]."</p>\n";
+	//$sabqueue = sabQuery("qstatus");
+	$sabqueueAdvanced = sabQuery("queue");
+	$sabqueue = $sabqueueAdvanced['queue'];
+	
+	if(!empty($_GET['debug']) && $_GET['debug']=='y') {
+		echo "<br/><pre>".print_r($sabqueue, true)."</pre>";
 	}
+
+	$ajaxurl = (!empty($_GET['style']) && ($_GET['style'] == "m") ? "index.php?w=wSabnzbd&" : "widgets/wSabnzbd.php?").(!empty($_GET['style']) ? "style=".$_GET['style']."&" : "")."c=".$count."&";
+	$pathtoimages = ((!empty($_GET['style']) && (($_GET['style'] == "m") || ($_GET['style'] == "s"))) ? "../" : "./");
+	$state = ($sabqueue["status"]);	
+
+	echo "\t\t<div id=\"sabheader\">\n";
+	if (strtolower($state) == "downloading") {
+		$cmdPauseResume = $ajaxurl."cmd=pause";
+		if(!empty($_GET['style']) && ($_GET['style'] == "w")) {
+			echo "\t\t\t<p><a href=\"#\" onclick=\"cmdSabnzbd('".$cmdPauseResume."');\">$state</a>";	
+		} else {
+			echo "\t\t\t<p><a href=\"".$cmdPauseResume."\" target=\"nothing\">$state</a>";	
+		}
+
+		//href link is the resume URL for all the queue
+		echo " AT ".$sabqueue["speed"]."</p>\n";
+		echo "\t\t\t<p>TIMELEFT - ".$sabqueue["timeleft"]."</p>\n";
+		$totalQ = (int)$sabqueue["mb"];
+		$remainingQ = (int)$sabqueue["mbleft"];
+		if($totalQ!=0){
+			$percentageQ = (int)((($totalQ - $remainingQ) / $totalQ)*100);
+			//Total progress bar with Time Left and MB left/ MB remainig as you can see from previous posts. It only shows up when the queue is not paused
+			echo "\t\t\t<div class=\"progressbar\"><div class=\"progress\" style=\"width:".$percentageQ."%\"></div><div class=\"progresslabel\">Time Left: ".$sabqueue["timeleft"]." TOTAL:".$sabqueue['sizeleft']."/".$sabqueue['size']."</div></div>\n";
+		}
+	} else {
+		//href link is the pause URL for all the queue
+		$cmdPauseResume = $ajaxurl."cmd=".((strtolower($state) == "paused") ? "resume" : "pause");
+		if(!empty($_GET['style']) && ($_GET['style'] == "w")) {
+			echo "\t\t\t<p><a href=\"#\" onclick=\"cmdSabnzbd('".$cmdPauseResume."');\">$state</a></p>\n";	
+		} else {
+			echo "\t\t\t<p><a href=\"".$cmdPauseResume."\" target=\"nothing\">$state</a></p>\n";	
+		}
+		echo "\t\t</div><!-- #sabheader -->\n";
+	}
+
 	$i = 0;
-	foreach($sabqueue["jobs"] as $slot) {
+	echo "\t\t<table>\n";
+	foreach($sabqueue["slots"] as $slot) {
 		if($i < $count) {
 			$total = (int)$slot["mb"];
 			$remaining = (int)$slot["mbleft"];
 			if($total > 0 && is_numeric($remaining)) {
 				$percentage = (int)((($total - $remaining) / $total)*100);
-				echo "\t<div class=\"progressbar\"><div class=\"progress\" style=\"width:".$percentage."%\"></div><div class=\"progresslabel\">".$slot["filename"]."</div></div><br/>\n";
+
+				// filename
+				$fullname = $slot["filename"];
+
+				//The sab ID to get individual pause/resume and delete 
+				$id = $slot["nzo_id"];
+
+				//the delete url for individual items
+				$cmdDelete = $ajaxurl."cmd=".urlencode("queue&name=delete&value=".$id);
+
+				//the current status for individual item
+				$status = ($slot["status"]);
+				if (strtolower($status) == "paused"){
+					//If paused $pause is the RESUME url (the button resumes the item)
+					$cmdPauseResume = $ajaxurl."cmd=".urlencode("queue&name=resume&value=".$id);
+					//When paused, adds PAUSED to the front of the name.
+					$name = "PAUSED - ".$fullname;
+				}
+				else{
+					// if not paused $pause is the PAUSE url
+					$cmdPauseResume = $ajaxurl."cmd=".urlencode("queue&name=pause&value=".$id);
+					$name = $fullname;
+				}
+				if(!empty($_GET['style']) && ($_GET['style'] == "w")) {
+					$actions = "<img src=\"".$pathtoimages."media/btnPlayPause.png\" onclick=\"cmdSabnzbd('".$cmdPauseResume."');\" />";
+					$actions .= "<img src=\"".$pathtoimages."media/queueDelete.png\" onclick=\"cmdSabnzbd('".$cmdDelete."');\" />";
+				} else {
+					$actions = "<a href=\"".$cmdPauseResume."\" target=\"nothing\"><img src=\"".$pathtoimages."media/btnPlayPause.png\" /></a>";
+					$actions .= "<a href=\"".$cmdDelete."\" target=\"nothing\"><img src=\"".$pathtoimages."media/btnQueueDelete.png\" /></a>";
+				}
+				echo "\t\t\t<tr class=\"queueitem\"><td><div class=\"progressbar\"><div class=\"progress\" style=\"width:".$percentage."%\"></div><div class=\"progresslabel\">".$name."</div></div></td><td class=\"actions\">".$actions."</td></tr>\n";
 			}
 		} else {
 			break;
 		}
 		$i += 1;
 	}
-	echo "</div><!-- sabnzbd -->\n";
+	echo "\t\t<table>\n";
+
+	echo "\t</div><!-- sabnzbd -->\n";
 }
 
-if (!empty($_GET['style']) && (($_GET['style'] == "w") || ($_GET['style'] == "s"))) {
+if(!empty($_GET['cmd'])) {
+	require_once "../config.php";
+
+	sabQuery(urldecode($_GET['cmd']));
+}
+
+if(!empty($_GET['style']) && (($_GET['style'] == "w") || ($_GET['style'] == "s"))) {
 	require_once "../config.php";
 
 	$count = (!empty($_GET['c'])) ? $_GET['c'] : 15;
-	if ($_GET['style'] == "w") {
+	if($_GET['style'] == "w") {
 ?>
 <html>
 	<head>
