f17886348fa458d1a4d5c9508e5e608e5f5d7089
nick8888 <nickshanley@gmail.com>
2010-11-16 10:56:05 +1100

    Sorry I added the wrong RSS widget

--- widgets/wRSS.php
+++ widgets/wRSS.php
@@ -2,56 +2,46 @@
 
 $wIndex["wRSS"] = array("name" => "RSS Feed", "type" => "inline", "function" => "widgetRSS();"); //Declare widget function
 
-function widgetRSS() { 
-	$xml = parseRSS("http://rss.nzbmatrix.com/rss.php?subcat=42");
+function widgetRSS() {
 
-	foreach($xml['RSS']['CHANNEL']['ITEM'] as $item) {
-	        echo("<p class=\"indexBoxNews\"><a href=\"{$item['LINK']}\" target=\"_blank\" class=\"indexBoxNews\">{$item['TITLE']}{$link}</a></p>");
+	$xml = "http://rss.nzbmatrix.com/rss.php?subcat=42";
+	$count = 10;
+
+	$rss = parseRSS($xml, $count); 
+
+	//output "<channel>" elements
+	echo "<p><a href='" . $rss['channellink'] . "'>" . $rss['channeltitle'] . "</a>";
+	echo "<br />"; 
+	//echo "<p>" . $rss['channeldesc'] . "</p>")
+
+	//output "<item>" elements
+	for ($i=0; $i<$count; $i++) {
+		echo "<p><a target=\"_blank\" href='" . $rss[$i]['itemlink'] . "'>" . $rss[$i]['itemtitle'] . "</a>";
+		echo "<br />";
+		//echo "<p>" . $rss[$i]['itemdesc'] . "</p>";
 	}
+
 }
-function parseRSS($url) { 
+
+function parseRSS ($xml, $count) {
  
-        $feedeed = implode('', file($url));
-        $parser = xml_parser_create();
-        xml_parse_into_struct($parser, $feedeed, $valueals, $index);
-        xml_parser_free($parser);
- 
-        foreach($valueals as $keyey => $valueal){
-            if($valueal['type'] != 'cdata') {
-                $item[$keyey] = $valueal;
-			}
-        }
- 
-        $i = 0;
- 
-        foreach($item as $key => $value){
- 
-            if($value['type'] == 'open') {
- 
-                $i++;
-                $itemame[$i] = $value['tag'];
- 
-            } elseif($value['type'] == 'close') {
- 
-                $feed = $values[$i];
-                $item = $itemame[$i];
-                $i--;
- 
-                if(count($values[$i])>1){
-                    $values[$i][$item][] = $feed;
-                } else {
-                    $values[$i][$item] = $feed;
-                }
- 
-            } else {
-                $values[$i][$value['tag']] = $value['value'];  
-            }
-        }
- 
-        return $values[0];
-} 
+	$xmlDoc = new DOMDocument();
+	$xmlDoc->load($xml);
+
+	//get "<channel>" elements
+	$channel = $xmlDoc->getElementsByTagName('channel')->item(0);
+	$rss['channeltitle'] = $channel->getElementsByTagName('title')->item(0)->childNodes->item(0)->nodeValue;
+	$rss['channellink'] = $channel->getElementsByTagName('link')->item(0)->childNodes->item(0)->nodeValue;
+	$rss['channeldesc'] = $channel->getElementsByTagName('description')->item(0)->childNodes->item(0)->nodeValue;
 
 
- 
-
+	//get "<item>" elements
+	$item = $xmlDoc->getElementsByTagName('item');
+	for ($i=0; $i<$count; $i++) {
+		$rss[$i]['itemtitle'] = $item->item($i)->getElementsByTagName('title')->item(0)->childNodes->item(0)->nodeValue;
+		$rss[$i]['itemlink'] = $item->item($i)->getElementsByTagName('link')->item(0)->childNodes->item(0)->nodeValue;
+		$rss[$i]['itemdesc'] = $item->item($i)->getElementsByTagName('description')->item(0)->childNodes->item(0)->nodeValue;	
+	}
+	return $rss;
+}
 ?>
