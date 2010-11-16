3f27cddae334b26f8b6120f4c42f086726b05e5c
nick8888 <nickshanley@gmail.com>
2010-11-16 18:47:26 +1100

    cleanup RSS widget

--- widgets/wRSS.php
+++ widgets/wRSS.php
@@ -3,116 +3,125 @@
 $wIndex["wRSS"] = array("name" => "RSS Feed", "type" => "inline", "function" => "widgetRSS();", "headerfunction" => "widgetRSSHeader();"); //Declare widget function
 
 function widgetRSSHeader() {
-	echo <<< RSSHEADER
-		<script type="text/javascript" language="javascript">
-		<!--
-			function showRSS(str) { 
-				if (str.length==0)   {  
-					document.getElementById("rssOutput").innerHTML=""; 
-					return; 
-				} 
-				if (window.XMLHttpRequest) {
-					// code for IE7+, Firefox, Chrome, Opera, Safari 
-					xmlhttp=new XMLHttpRequest(); 
-				} else {
-					// code for IE6, IE5 
-					xmlhttp=new ActiveXObject("Microsoft.XMLHTTP"); 
-				} 
-				xmlhttp.onreadystatechange = function() { 
-					if (xmlhttp.readyState==4 && xmlhttp.status==200) { 
-						document.getElementById("rssOutput").innerHTML=xmlhttp.responseText; 
-					} 
-				} 
-				xmlhttp.open("GET","widgets/wRSS.php?style=s&rss="+str,true); 
-				xmlhttp.send(); 
-			} 
-		-->
-		</script>
-
+        echo <<< RSSHEADER
+<script type="text/javascript" language="javascript">
+function showRSS(str) {
+	if (str.length==0) {
+		document.getElementById("rssOutput").innerHTML="";
+		return;
+	}
+	if (window.XMLHttpRequest)
+		{// code for IE7+, Firefox, Chrome, Opera, Safari
+		xmlhttp=new XMLHttpRequest();
+	} else
+		{// code for IE6, IE5
+		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	xmlhttp.onreadystatechange=function() {
+		if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+			document.getElementById("rssOutput").innerHTML=xmlhttp.responseText;
+		}
+	}
+	xmlhttp.open("GET","widgets/wRSS.php?feed="+str,true);
+	xmlhttp.send();
+}
+</script>
 RSSHEADER;
 }
 
-function widgetRSS() {
-	global $rssfeeds;
-	echo "10 Most Recent - <form>\n"; 
-	echo "\t<select onchange=\"showRSS(this.value)\">\n"; 
-	echo "\t\t<option value=\"\">Select an RSS-feed:</option>\n"; 
-	foreach($rssfeeds as $name => $feed) {
-		echo "\t\t<option value=\"".$name."\">".$name."</option>\n"; 
-	}
-	echo "\t</select>\n"; 
-	echo "</form>\n"; 
-	echo "<br />\n"; 
-	echo "<div id=\"rssOutput\">RSS-feed will take a few seconds to load...</div>\n";
+function widgetRSS($feed) {
+	echo "<form>\n";
+	echo "<select onchange=\"showRSS(this.value)\">\n";
+	echo "<option value=\"\">Select an RSS-feed:</option>\n";
+	echo "<option value=\"NZBMatrix - TV Shows (DivX)\">NZBMatrix - TV Shows (DivX)</option>\n";
+	echo "<option value=\"NZBMatrix - TV Shows (HD x264)\">NZBMatrix - TV Shows (HD x264)</option>\n";
+	echo "<option value=\"NZBMatrix - Movies (DivX)\">NZBMatrix - Movies (DivX)</option>\n";
+	echo "<option value=\"NZBMatrix - Movies (HD x264)\">NZBMatrix - Movies (HD x264)</option>\n";
+	echo "<option value=\"NZBMatrix - Music (MP3)\">NZBMatrix - Music (MP3)</option>\n";
+	echo "<option value=\"NZBMatrix - Music (Loseless)\">NZBMatrix - Music (Loseless)</option>\n";
+	echo "</select>\n";
+	echo "</form>\n";
+
+	$xml = getRSSPath($feed);
+	parseRSS($xml);
 }
-function displayRSS($url, $count = 10, $returnonly = false) {
-	$return = "";
-	if(!empty($url)) {
 
-		$xmlDoc = new DOMDocument();
-		$xmlDoc->load($url);
-
-		//get elements from "<channel>"
-		$channel = $xmlDoc->getElementsByTagName('channel')->item(0);
-		//$channel_title = $channel->getElementsByTagName('title')->item(0)->childNodes->item(0)->nodeValue;
-		//$channel_link = $channel->getElementsByTagName('link')->item(0)->childNodes->item(0)->nodeValue;
-		//$channel_desc = $channel->getElementsByTagName('description')->item(0)->childNodes->item(0)->nodeValue;
-
-		//output elements from "<channel>"
-		//$return .= "<p><a href='".$channel_link."'>".$channel_title."</a></p>");
-
-		//get and output "<item>" elements
-		$x = $xmlDoc->getElementsByTagName('item');
-		$alt = false;
-		for ($i=0; $i<$count; $i++){
-			$item_title = $x->item($i)->getElementsByTagName('title')->item(0)->childNodes->item(0)->nodeValue;
-			$item_link = $x->item($i)->getElementsByTagName('link')->item(0)->childNodes->item(0)->nodeValue;
-			$item_desc = $x->item($i)->getElementsByTagName('description')->item(0)->childNodes->item(0)->nodeValue;
-			$item_desc = str_replace("'", "", str_replace("\"", "'", $item_desc));
-			$return .= ("<p class=\"".($alt ? " alt" : "")."\"><a href=\"".$item_link."\" target=\"_blank\" onMouseOver=\"ShowPopupBox('".$item_desc."');\" onMouseOut=\"HidePopupBox();\">".$item_title."</a></p>");
-			$alt = !$alt;
+//Use specified feed otherwise use default
+function getRSSPath ($feed) {   
+	if (!empty($feed)) {
+		switch ($feed) {
+			case "NZBMatrix - TV Shows (DivX)":
+				$xml=("http://rss.nzbmatrix.com/rss.php?subcat=6");
+				break;
+			case "NZBMatrix - TV Shows (HD x264)":
+				$xml=("http://rss.nzbmatrix.com/rss.php?subcat=41");
+				break;
+			case "NZBMatrix - Movies (DivX)":
+				$xml=("http://rss.nzbmatrix.com/rss.php?subcat=2");		
+				break;
+			case "NZBMatrix - Movies (HD x264)":
+				$xml=("http://rss.nzbmatrix.com/rss.php?subcat=42");
+				break;
+			case "NZBMatrix - Music (MP3)":
+				$xml=("http://rss.nzbmatrix.com/rss.php?subcat=22");
+				break;
+			case "NZBMatrix - Music (Loseless)":
+				$xml=("http://rss.nzbmatrix.com/rss.php?subcat=23");
+				break;
 		}
 	} else {
-		$return = "<p>No RSS feed supplied.</p>";
+		$xml = ("http://rss.nzbmatrix.com/rss.php?subcat=42");
 	}
-
-	if(!$returnonly) {
-		echo $return;
-	}
-	return $return;
+	return $xml;
 }
 
-if(!empty($_GET['style']) && (($_GET['style'] == "w") || ($_GET['style'] == "s"))) {
-	require_once "../config.php";
-	global $rssfeeds;
+function parseRSS ($xml) {
+ 
+	$xmlDoc = new DOMDocument();
+	$xmlDoc->load($xml);
 
-	$count = (!empty($_GET['c'])) ? $_GET['c'] : 10;
-	if(!empty($_GET['rss']) || !empty($_GET['rssurl'])) {
-		if(!empty($_GET['rssurl'])) {
-			$url = $_GET['rssurl'];
-		} else {
-			$url = (!empty($rssfeeds[$_GET['rss']]) ? $rssfeeds[$_GET['rss']] : "");
-		}
+	//get "<channel>" elements
+	//$channel = $xmlDoc->getElementsByTagName('channel')->item(0);
+	//$rss['channeltitle'] = $channel->getElementsByTagName('title')->item(0)->childNodes->item(0)->nodeValue;
+	//$rss['channellink'] = $channel->getElementsByTagName('link')->item(0)->childNodes->item(0)->nodeValue;
+	//$rss['channeldesc'] = $channel->getElementsByTagName('description')->item(0)->childNodes->item(0)->nodeValue;
+
+	//Get number of items to return
+	if (!empty($_GET['c'])) {
+		$count = $_GET['c'];
 	} else {
-		$url = reset($rssfeeds);
+		$count = 10;
 	}
-	if($_GET['style'] == "w") {
-?>
-<html>
-	<head>
-		<title>Media Front Page - RSS Feed</title>
-		<link rel='stylesheet' type='text/css' href='css/front.css'>
-	</head>
-	<body>
-<?php
-		displayRSS($url);
-?>
-	</body>
-</html>
-<?php
-	} else {
-		displayRSS($url);
+
+	//get "<item>" elements
+	$item = $xmlDoc->getElementsByTagName('item');
+	for ($i=0; $i<$count; $i++) {
+		$rss[$i]['itemtitle'] = $item->item($i)->getElementsByTagName('title')->item(0)->childNodes->item(0)->nodeValue;
+		$rss[$i]['itemlink'] = $item->item($i)->getElementsByTagName('link')->item(0)->childNodes->item(0)->nodeValue;
+		$rss[$i]['itemdesc'] = $item->item($i)->getElementsByTagName('description')->item(0)->childNodes->item(0)->nodeValue;	
 	}
+
+	//output items
+	echo "<div id=\"rssOutput\">\n";
+	//output "<channel>" elements
+	//echo "<p><a href='" . $rss['channellink'] . "'>" . $rss['channeltitle'] . "</a>";
+	//echo "<br />"; 
+	//echo "<p>" . $rss['channeldesc'] . "</p>")
+
+	//output "<item>" elements
+	for ($i=0; $i<$count; $i++) {
+		echo "<p><a target=\"_blank\" href='" . $rss[$i]['itemlink'] . "'>" . $rss[$i]['itemtitle'] . "</a>";
+		echo "<br />";
+		//echo "<p>" . $rss[$i]['itemdesc'] . "</p>";
+	}
+	echo "</div>\n";
 }
-  
+
+//if called with parameter load specified feed
+if (!empty($_GET['feed'])) {
+	$feed = $_GET['feed'];
+	$xml = getRSSPath($feed);
+	parseRSS($xml);
+}
+
 ?>
