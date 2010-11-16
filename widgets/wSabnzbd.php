4c73cb2394581155ed98775e1662a11bcef13914
Rodney Shupe <rodney@shupe.ca>
2010-11-16 00:18:18 -0800

    Fixed sabnzbd refresh.

--- widgets/wSabnzbd.php
+++ widgets/wSabnzbd.php
@@ -1,5 +1,5 @@
 <?php
-$wdgtSabnzbd = array("name" => "Sabnzbd", "type" => "ajax", "block" => "sabnzbdwrapper", "headerfunction" => "widgetSabnzbdHeader();", "call" => "widgets/wSabnzbd.php?style=w&c=15", "interval" => 900000);
+$wdgtSabnzbd = array("name" => "Sabnzbd", "type" => "ajax", "block" => "sabnzbdwrapper", "headerfunction" => "widgetSabnzbdHeader();", "call" => "widgets/wSabnzbd.php?style=w&c=15", "interval" => 10000);
 $wIndex["wSabnzbd"] = $wdgtSabnzbd;
 
 function widgetSabnzbdHeader() {
