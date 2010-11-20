e399e2a0dfd8690abc3cc865ce1ab0e4bc2f05cc
nick8888 <nickshanley@gmail.com>
2010-11-20 12:10:25 +1100

    cleanup

--- test-front.php
+++ test-front.php
@@ -14,20 +14,18 @@ error_reporting($errlevel);
 		include_once $filename;
 
 		// Create widget
-		//$$widget_init['Id'] = new widget ($widget_init['Id'], $widget_init['Type'], $widget_init['Block'], $widget_init['Title'], $widget_init['Function'], $widget_init['HeaderFunction'], $widget_init['Section'], $widget_init['Position']);
-
 		$$widget_init['Id'] = new widget ($widget_init);
+
 		// Add widget to database
 		$$widget_init['Id']->addWidget();
 		$$widget_init['Id']->updateWidget('Title', 'Hard Drives');
+
 		// Retrieve widget from database
 		$wdgt = $$widget_init['Id']->getWidget();
 		echo $wdgt['Id'];
 		echo $wdgt['Function'];
 		echo $wdgt['Title'];
 	}    	
-
-
 ?>
 <html>
 	<head>
